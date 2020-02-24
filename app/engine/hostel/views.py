bd7fb0663fa09ff367962711ad79605dc1230b7d
Sablin Roman <roman.sablinv@yandex.ru>
2020-02-24 18:15:15 +0700

    fix cards view

--- app/engine/hostel/views.py
+++ app/engine/hostel/views.py
@@ -20,39 +20,36 @@ def number_of(request):
 														'female_places' : female_places,
 														'save_places' : save_places,
 														'empty_places' : empty_places,
-														'country': county, 'faculty': faculty})
-
-
+														'country': county, 
+														'faculty': faculty})
 
 
 def cards(request):
 	rooms = Student.objects.all()
+	status_list = ['мужское', 'женское', 'занято', 'пусто']
 
 	last_room = None
 	cards_dict = {}
-	place_list = []
-	name_list = []
-
+	content_list = []
 
 	for i in rooms:
-
-		if i.place_status == 'мужское' or i.place_status == 'женское' or i.place_status == 'занято':
-			place_list.append(i.place_status)
-
-
 		if last_room == None:
-			last_room = i.room
-			name_list.append(i.name)
+			last_room = i.room 
 
+		if i.room == last_room:
+			if i.place_status in status_list:
+				content_list.append(i.place_status)
+			else:
+				content_list.append(i.name)
 
-		elif i.room == last_room:
-			name_list.append(i.name)
 		else:
-			cards_dict[last_room] = name_list
-			print(place_list)
-			name_list = place_list = []
-			name_list.append(i.name)
-			last_room = i.room
-
+			cards_dict[last_room] = content_list
+			last_room = i.room 
+			content_list = []
+			if i.place_status in status_list:
+				content_list.append(i.place_status)
+			else:
+				content_list.append(i.name)
+	cards_dict[last_room] = content_list
 
 	return  render(request, 'hostel/cards.html', context={'cards' : cards_dict})
